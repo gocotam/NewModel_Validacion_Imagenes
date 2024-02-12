f0e61929e23e6855076b34d559989674851ae0a1
JESÚS SALVADOR CAMACHO HERRERA <jscamachoh@820Q9K7WGL7WQs-MacBook-Pro.local>
2024-02-12 16:27:01 -0600

    update3

---
+++ valid_enriq/funcion_endpoints.py
@@ -0,0 +1,12 @@
+def endpoint_validacion():
+    project = "209565165407"
+    endpoint_id = "1630331652410441728"
+    location = "us-central1"
+    return project, endpoint_id, location
+
+def endpoint_enriquecimiento():
+    project = "209565165407"
+    endpoint_id = "9120899752169308160"
+    location = "us-central1"
+    return project, endpoint_id, location
+
